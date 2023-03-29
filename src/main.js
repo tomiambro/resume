333495429c53983056ba1c94bda441722221cfc8
Tomas Ambrogi <tomi.ambro94@gmail.com>
2023-03-29 20:30:44 -0300

    adding firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,23 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+// import './firebase.js'
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDUsU9DH82b2UkFYQWe26rUK3skgMFptgM",
+    authDomain: "tomashoracioambrogi.firebaseapp.com",
+    projectId: "tomashoracioambrogi",
+    storageBucket: "tomashoracioambrogi.appspot.com",
+    messagingSenderId: "1074284210809",
+    appId: "1:1074284210809:web:5075cf555cd9e461b4c2a8",
+    measurementId: "G-9S3SKBCTKN"
+  };
+
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const app = createApp(App)
 
