b12c269e91232850597540c74d18de27b712dd75
Tomas Ambrogi <tomi.ambro94@gmail.com>
2023-03-29 22:26:49 -0300

    formatting

--- src/main.js
+++ src/main.js
@@ -4,21 +4,21 @@ import router from './router'
 
 import './assets/main.css'
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDUsU9DH82b2UkFYQWe26rUK3skgMFptgM",
-    authDomain: "tomashoracioambrogi.firebaseapp.com",
-    projectId: "tomashoracioambrogi",
-    storageBucket: "tomashoracioambrogi.appspot.com",
-    messagingSenderId: "1074284210809",
-    appId: "1:1074284210809:web:5075cf555cd9e461b4c2a8",
-    measurementId: "G-9S3SKBCTKN"
-  };
+    apiKey: 'AIzaSyDUsU9DH82b2UkFYQWe26rUK3skgMFptgM',
+    authDomain: 'tomashoracioambrogi.firebaseapp.com',
+    projectId: 'tomashoracioambrogi',
+    storageBucket: 'tomashoracioambrogi.appspot.com',
+    messagingSenderId: '1074284210809',
+    appId: '1:1074284210809:web:5075cf555cd9e461b4c2a8',
+    measurementId: 'G-9S3SKBCTKN'
+}
 
-const firebaseApp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(firebaseApp);
+const firebaseApp = initializeApp(firebaseConfig)
+const analytics = getAnalytics(firebaseApp)
 
 const app = createApp(App)
 
